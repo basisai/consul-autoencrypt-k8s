1d78eb0b81c93d162554f6763e6cc250c7edf97b
Yong Wen Chua <lawliet89@users.noreply.github.com>
2020-10-14 11:19:14 +0800

    Move files to Helm directory structure (#3)
    * Move files to Helm directory structure
    
    * Fix test directory
    
    * Again

--- tests/test.sh
+++ tests/test.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 BATS_IMAGE="${BATS_IMAGE:-basisai/bats-helm:v1.2.1}"
 
-docker run --rm -v "$(git rev-parse --show-toplevel):/tests" --workdir "/tests" \
+docker run --rm -v "$(git rev-parse --show-toplevel)/charts/consul-autoencrypt-k8s:/tests" --workdir "/tests" \
     "${BATS_IMAGE}" \
     bats tests/unit
